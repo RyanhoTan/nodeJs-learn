4a35100401c17d5c831cb5053ca359416439486e
tyh <2939056298@qq.com>
2026-06-09 11:51:19 +0800

    学习了readfile()

---
+++ readfile.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.readFile('halo.js','utf-8', function(err, data){
+    // console.log(err.message);
+    // console.log('----------------------------');
+    // console.log(data);
+    if(err) {
+        console.log('读取文件失败'+ err.message);
+    } 
+        console.log('读取文件成功:');
+        console.log(data);
+    
+})

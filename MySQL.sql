dde3ad114b7a51f0a7932b8e6ef6cdf7f036f824
tyh <2939056298@qq.com>
2026-06-12 17:00:23 +0800

    学习了MySQL基础知识

---
+++ MySQL.sql
@@ -0,0 +1,19 @@
+-- select * from users
+
+-- select username, password from users
+
+-- insert into users (username, password) values ('hoooooooooo', 'ryanusersusersididusernamepassword')
+
+-- update users set username='root', password='admin123', status='1' where id=1
+
+-- select * from users
+
+-- delete from users where id=1
+
+-- select * from users where password='123456'
+
+-- select * from users where id>3 or username='ryanho'
+
+-- select password as pwd from users
+
+select count(*) from users where password='123456'

cc7a7bde0776d7ab4eca5c737301eb17b4110059
tyh <2939056298@qq.com>
2026-06-14 10:02:27 +0800

    学习了update数据的快捷方式

--- mysql/update.js
+++ mysql/update.js
@@ -10,9 +10,14 @@ const db = mysql.createPool({
 
 const user = { id: 2, username: 'updateName', password: 'updatePassword' }
 
-const sqlStr = 'update users set username=?, password=? where id=?'
+// const sqlStr = 'update users set username=?, password=? where id=?'
 
-db.query(sqlStr, [user.username, user.password, user.id],  (err, res) => {
+// 快捷方式，直接set ? 占位，后面在数组里加上对象
+const sqlStr = 'update users set ? where id=?'
+
+db.query(sqlStr, [user, user.id],  (err, res) => {
+// db.query(sqlStr, [user.username, user.password, user.id],  (err, res) => {
+    
     if (err) return console.log(err.message)
         if (res.affectedRows === 1) {
             console.log(res)
